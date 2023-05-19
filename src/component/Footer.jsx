17143cd61aaeefc8ec456f03d4908dd2bd18aadf
rasyadmar <rasyadmar@gmail.com>
2023-05-19 07:40:54 +0700

    adding footer view

---
+++ src/component/Footer.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import "./style/Footer.css"
+
+export default function Footer() {
+  return (
+    <>
+    <div id="footer" className="d-flex flex-row  justify-content-center">
+    <p className="color-black my-2">Created by Humble Man. © 2020</p>
+    </div>
+      
+    </>
+  );
+}
